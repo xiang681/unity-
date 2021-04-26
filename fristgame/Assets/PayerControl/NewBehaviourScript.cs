4c1549912e84e427c7b7b750632fe2a8515d338e
xiang681 <3234340540@qq.com>
2021-04-26 23:29:21 +0800

    first

---
+++ fristgame/Assets/PayerControl/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D heroBody;
+    public float force = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        heroBody.AddForce(Vector2.right * h * force);
+    }
+}
