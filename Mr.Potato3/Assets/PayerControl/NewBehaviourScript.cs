ac6ec503e8dbea8ba7cfdcc5679d4b839733df8f
xiang681 <3234340540@qq.com>
2021-05-11 21:45:58 +0800

    unity

---
+++ Mr.Potato3/Assets/PayerControl/NewBehaviourScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D heroBody;
+    public float force = 100;
+    public float jumpForce = 500;
+    float fInput = 0.0f;
+    public float MaxSpeed = 5;
+    private bool bFaceRight=true;
+    private bool bGrounded = false;
+    Transform mGroundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+        mGroundCheck = transform.Find("GroundCheck");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fInput = Input.GetAxis("Horizontal");
+        if(fInput<0&&bFaceRight)
+        {
+            flip();
+        }
+        else if(fInput > 0 && !bFaceRight)
+                {
+            flip();
+        }
+        //heroBody.AddForce(Vector2.right * h * force);
+
+        bGrounded = Physics2D.Linecast(transform.position, mGroundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+    }
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(heroBody.velocity.x) < MaxSpeed)
+            heroBody.AddForce(fInput * force * Vector2.right);
+
+        if (Mathf.Abs(heroBody.velocity.x) > MaxSpeed)
+            heroBody.velocity = new Vector2(Mathf.Sign(heroBody.velocity.x) * MaxSpeed, heroBody.velocity.y);
+        bool bjump = false;
+        if(bGrounded)
+        {
+            bjump = Input.GetKeyDown(KeyCode.Space);
+            Vector2 upForce = new Vector2(0, 1);
+            if(bjump)
+                heroBody.AddForce(upForce*jumpForce);
+
+            
+        }
+    }
+
+    void flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        bFaceRight = !bFaceRight;
+    }
+    
+}
