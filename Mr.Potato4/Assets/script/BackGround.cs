5683009835aef652ea9dc21c721adf41dc24bf2d
xiang681 <3234340540@qq.com>
2021-05-19 22:12:29 +0800

    Mr.Potato4

---
+++ Mr.Potato4/Assets/script/BackGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] backGrounds;
+    public float fparallax = 0.4f;
+    public float layerFraction = 5f;
+    public float fSmooth = 5f;
+    
+    Transform cam;
+    Vector3 previousCamPos;
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    private void Start()
+    {
+        previousCamPos = cam.position;
+    }
+    
+    void Update()
+    {
+        float fParrlaxX = (previousCamPos.x - cam.position.x) * fparallax; //计算相机运动量
+        for (int i=0;i< backGrounds.Length;i++)
+        {
+            float fNewX = backGrounds[i].position.x + fParrlaxX * (1 + i * layerFraction);//计算各层的运动量
+            Vector3 newPos = new Vector3(fNewX, backGrounds[i].position.y, backGrounds[i].position.z);
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, newPos, fSmooth*Time.deltaTime) ;
+        }
+        previousCamPos = cam.position;
+    }
+}
---
+++ Mr.Potato4/Assets/script/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Rigidbody2D rocket;
+    public float fSpeed = 10;
+    PlayerControl playerCtrl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCtrl = transform.root.GetComponent<PlayerControl>();
+        //rocket = Resources.Load<GameObject>(path: "rocket");
+    }
+        // Update is called once per frame
+        void Update()
+        {
+            //if(Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Vector3 direction = new Vector3(0, 0, 0);
+                if (playerCtrl.bFaceRight)
+                {
+                    Rigidbody2D RockectInstance = Instantiate(rocket, transform.position, Quaternion.Euler(direction));
+                    RockectInstance.velocity = new Vector2(fSpeed, 0);
+                }
+                else
+                {
+                    direction.z = 180;
+                    Rigidbody2D RockectInstance = Instantiate(rocket, transform.position, Quaternion.Euler(direction));
+                    RockectInstance.velocity = new Vector2(-fSpeed, 0);
+                }
+            }
+        }
+    }
+
